29e6c72dc13d81460a50de8df2c4525474afd1d7
abdomohamed911 <124688183+abdomohamed911@users.noreply.github.com>
2024-10-12 08:04:29 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.write('Hello in JS');
+
+let x= 10;
+x= "Computer Science ID";
+
+let y= 10;
+y="Computer Science"
+
+console.log(`My id is ${x}`);
+console.log(+`Iam always good at ${y}`);
